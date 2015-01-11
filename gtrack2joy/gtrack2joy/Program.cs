9eec9f08ec3c3e5aa0165cfd7d6849234df4cb31
Michał Janczykowski <mjanczykowski@gmail.com>
2015-01-11 21:59:37 +0100

    created C# project for gtrack2joy transmission app; currently dummy app updating single axis of v-joy with decrementing value

---
+++ gtrack2joy/gtrack2joy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gtrack2joy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vjoy = new VJoy();
+
+            vjoy.Initialize();
+            vjoy.Reset();
+            vjoy.Update(0);
+
+            short x = 0;
+
+            while (true)
+            {
+                vjoy.SetXAxis(0, x);
+                vjoy.Update(0);
+                System.Threading.Thread.Sleep(100);
+                x--;
+            }
+
+        }
+    }
+}
